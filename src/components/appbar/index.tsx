01941a1914c8e4b01600a13ab6481c73d139f6d5
renancarvalhops <renancps77@gmail.com>
2025-12-25 21:09:29 -0300

    style: cor do título do appbar

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -1,26 +1,26 @@
+import { HexadecimalColors } from "../graph-viewer/ViewerUtils";
 import About from "./About";
-import DownloadColoredGraph from "./DownloadColoredGraph";
 import DownloadGraph6 from "./DownloadGraph6";
 import Instructions from "./Instructions";
 import NewGraph from "./NewGraph";
 import ShowColoring from "./ShowColoring";
 
 export default function AppBar() {
+    const title = "Total-Color";
+
     return (
         <menu className="bg-blue-500 flex flex-col gap-4 items-center justify-between lg:flex-row px-12 py-4 relative shadow">
 
             <h2 className="flex gap-[0.1rem] select-none text-2xl text-white">
-                <span className="hover:text-red-500">T</span>
-                <span className="hover:text-lime-500">o</span>
-                <span className="hover:text-teal-500">t</span>
-                <span className="hover:text-emerald-500">a</span>
-                <span className="hover:text-indigo-500">l</span>
-                <span className="hover:text-rose-500">-</span>
-                <span className="hover:text-slate-500">C</span>
-                <span className="hover:text-yellow-500">o</span>
-                <span className="hover:text-cyan-500">l</span>
-                <span className="hover:text-green-500">o</span>
-                <span className="hover:text-amber-500">r</span>
+                {title.split("").map((char, index) => (
+                    <span
+                        key={index}
+                        onMouseEnter={(e) => e.currentTarget.style.color = HexadecimalColors.get(index)}
+                        onMouseLeave={(e) => e.currentTarget.style.color = ""}
+                    >
+                        {char}
+                    </span>
+                ))}
             </h2>
 
             <div className="flex flex-wrap gap-2">
